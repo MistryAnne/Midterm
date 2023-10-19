da2ec6cdad15a50100ffb33f66ec8e182c12f96e
Anne Mistry <amistr34@my.centennialcollege.ca>
2023-10-18 20:24:40 -0400

    Created initial react

---
+++ midterm/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function Submit(){
+  return(
+    <button>
+      Submit
+    </button>
+  )
+}
+
+export default function App(){
+  return (
+    <div>
+      <h1>New Product</h1>
+      <Submit />
+    </div>
+  )
+}
